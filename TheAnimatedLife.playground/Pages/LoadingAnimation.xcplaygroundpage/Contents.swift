518fa2fd62e0f22e12b2096888041e0db10b921c
Stephen Martinez <enharmonicnuance@gmail.com>
2019-05-04 20:26:32 -0700

    Added some comments and the first of the rendered text

--- TheAnimatedLife.playground/Pages/LoadingAnimation.xcplaygroundpage/Contents.swift
+++ TheAnimatedLife.playground/Pages/LoadingAnimation.xcplaygroundpage/Contents.swift
@@ -1,27 +1,32 @@
 import PlaygroundSupport
 import UIKit
 
+// This allows us to do run time-based executions
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+// Create the LoadingExample which is a `UINavigationController`
 let loadingExample = LoadingExample.create()
 
+// This assigns the loadingExample as the Live View to the right
 PlaygroundPage.current.liveView = loadingExample
 
+// Example controller to do our animations in.
 class AnimatedLoginController: LoginViewController {
-    
+//: Provide your animation implementation here in `runLoadingAnimation`.
     override func runLoadingAnimation(logo: UIView, background: UIView) {
         
         let degrees = CGFloat(180 * Double.pi / 180)
+        let transform2D = CGAffineTransform(scaleX: 3, y: 3)
+        let transform3D = CATransform3DMakeRotation(degrees, 0, 1, 0)
         
         UIView.animate(
             withDuration: 1.0,
             delay: 0,
             options: .curveEaseOut,
             animations: {
-                let transform = CGAffineTransform(scaleX: 3, y: 3)//.rotated(by: degrees)
-                let transform3D = CATransform3DMakeRotation(degrees, 0, 1, 0)
-                logo.transform = transform
-                logo.layer.transform = CATransform3DConcat(logo.layer.transform, transform3D)
+                logo.transform = transform2D
+                let combined2DAnd3D = CATransform3DConcat(logo.layer.transform, transform3D)
+                logo.layer.transform = combined2DAnd3D
                 logo.alpha = 0
         },
             completion: nil
@@ -41,6 +46,7 @@ class AnimatedLoginController: LoginViewController {
     
 }
 
+// This simulates the completion of Loading an app.
 executeAfter(2) {
     loadingExample.pushViewController(AnimatedLoginController(), animated: false)
 }
